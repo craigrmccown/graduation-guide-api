b6e8390ea4b5f1ffdba88163030916213d3e4d23
Craig McCown <craigrmccown@gmail.com>
2016-02-10 13:04:27 -0500

    added catch all 404 route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   post 'auth/login' => 'auth#login'
   post 'auth/logout' => 'auth#logout'
   post 'users' => 'user#register_student'
+  match '*path', to: 'application#missing_endpoint', via: :all
 end
