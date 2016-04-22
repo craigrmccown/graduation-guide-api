1f8e57bffbb5089f2c4183166fb4316601a59607
Craig McCown <craigrmccown@gmail.com>
2016-04-22 18:30:50 -0400

    loading grouch data in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,11 @@ def safe_create(model)
   else
     model.save!
   end
+
+  model
 end
 
+# Delete prerequisites recursively
 def delete_prereqs(course)
   return if course.prereq_id.nil?
 
@@ -42,50 +45,62 @@ def delete_prereqs(course)
   ActiveRecord::Base.connection.execute query
 end
 
+# Insert prerequisites recursively
 def insert_prereqs(course, data, parent_id=nil)
   prereq = Prereq.new op: data['type'], parent_id: parent_id
   prereq.save!
 
-  outfile = File.open('data/seeds.out', 'a')
-
-  begin
-    data['courses'].each do |prereq_course_data|
-      if prereq_course_data.is_a?(String)
-        begin
-          course = Course.find_by! name: prereq_course_data
-          prereq.courses << course
-        rescue
-          outfile.puts "Could not find course with name #{prereq_course_data}"
-        end
-      else
-        insert_prereqs(course, prereq_course_data, prereq.id)
+  data['courses'].each do |prereq_course_data|
+    if prereq_course_data.is_a?(String)
+      begin
+        course = Course.find_by! name: prereq_course_data
+        prereq.courses << course
+      rescue
+        puts "Could not find course with name #{prereq_course_data}"
       end
+    else
+      insert_prereqs(course, prereq_course_data, prereq.id)
     end
-  ensure
-    outfile.close
   end
 
   prereq
 end
+
+# Clear output file
+File.open('data/seeds.out', 'w').close
 
 # Create static data
 role_admin = safe_create Role.new(id: 0, name: 'admin')
 role_student = safe_create Role.new(id: 1, name: 'student')
 major_cs = safe_create Major.new(id: 0, name: 'Computer Science', description: 'Georgia Tech computer science major')
 
-# Get grouch course ids
-major_id = 'CS'
-grouch_course_ids = Grouch::get_course_ids major_id
+# Get grouch school ids
+school_ids = Grouch::get_school_ids
+grouch_course_ids = []
 
-# Get courses in Grouch not yet in database
-existing_course_ids = Course.all.collect { |course| course.id }
-new_course_ids = grouch_course_ids - existing_course_ids
+# Insert courses for each school
+school_ids.each do |school_id|
 
-# Insert new courses
-new_course_ids.each do |course_id|
-  data = Grouch::get_course major_id, course_id
-  course = Course.new id: course_id, name: data['identifier'], description: data['name'], grouch_data: data.to_json
-  major_cs.courses << course
+  begin
+    # Get courses in Grouch not yet in database
+    grouch_course_ids = Grouch::get_course_ids school_id
+    existing_course_ids = Course.all.collect { |course| course.id }
+    new_course_ids = grouch_course_ids - existing_course_ids
+
+    # Insert new courses
+    new_course_ids.each do |course_id|
+      begin
+        data = Grouch::get_course school_id, course_id
+      rescue Exception => e
+        puts "#{e.message}, #{e.backtrace}"
+      end
+
+      course = Course.new id: course_id, name: data['identifier'], description: data['name'], grouch_data: data.to_json
+      course.save!
+    end
+  rescue Exception => e
+    puts "#{e.message}, #{e.backtrace}"
+  end
 end
 
 # Index courses by name
@@ -93,8 +108,6 @@ courses_by_name = {}
 Course.all.each { |course| courses_by_name[course.name] = course }
 
 # Resolve prereqs
-File.open('data/seeds.out', 'w').close
-
 courses_by_name.each do |course_name, course|
   grouch_data = JSON.parse(course.grouch_data)
   next if grouch_data['prerequisites'].nil?
--- lib/grouch.rb
+++ lib/grouch.rb
@@ -12,20 +12,27 @@ module Grouch
       response = Net::HTTP.get_response URI(response.header['location'])
     end
 
+    if response.code.eql? "500"
+      raise "500: #{path}"
+    end
+
     JSON.parse response.body
   end
 
-  def self.get_major_ids(semester_id="spring2016")
+  def self.get_school_ids(semester_id="spring2016")
     data = grouch_get semester_id
+    raise "Could not find key 'schools', path was #{semester_id}, data was #{data}" if data['schools'].nil?
     data['schools']
   end
 
-  def self.get_course_ids(major_id, semester_id="spring2016")
-    data = grouch_get "#{semester_id}/#{major_id}"
+  def self.get_course_ids(school_id, semester_id="spring2016")
+    path = "#{semester_id}/#{school_id}"
+    data = grouch_get path
+    raise "Could not find key 'numbers', path was #{path}, data was #{data}" if data['numbers'].nil?
     data['numbers'].collect { |number| number.to_i }
   end
 
-  def self.get_course(major_id, course_id, semester_id="spring2016") 
-    grouch_get "#{semester_id}/#{major_id}/#{course_id}"
+  def self.get_course(school_id, course_id, semester_id="spring2016") 
+    grouch_get "#{semester_id}/#{school_id}/#{course_id}"
   end
 end
