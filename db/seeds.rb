1f4e6d28c2c9d0598feb4455b21de85052d20e2f
Craig McCown <craigrmccown@gmail.com>
2016-04-24 08:06:08 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,19 +29,29 @@ course_germ2001 = safe_create Course.new(id: 2, name: 'GERM 2001', description: 
 course_germ2002 = safe_create Course.new(id: 3, name: 'GERM 2002', description: 'German II', hours: 3, course_groups: [course_group_humanities])
 course_span2001 = safe_create Course.new(id: 4, name: 'SPAN 2001', description: 'Spanish I', hours: 3, course_groups: [course_group_humanities])
 course_span2002 = safe_create Course.new(id: 5, name: 'SPAN 2002', description: 'Spanish II', hours: 3, course_groups: [course_group_humanities])
-course_phys2211 = safe_create Course.new(id: 6, name: 'PHYS 2211', description: 'Physics I', hours: 4, course_groups: [course_group_lab_sciences])
-course_phys2212 = safe_create Course.new(id: 7, name: 'PHYS 2212', description: 'Physics II', hours: 4, course_groups: [course_group_lab_sciences])
-course_chem1310 = safe_create Course.new(id: 8, name: 'CHEM 1310', description: 'General Chemistry', hours: 4, course_groups: [course_group_lab_sciences])
-course_chem1211 = safe_create Course.new(id: 9, name: 'CHEM 1211', description: 'Chemistry I', hours: 4, course_groups: [course_group_lab_sciences])
-course_chem1212 = safe_create Course.new(id: 10, name: 'CHEM 1212', description: 'Chemistry II', hours: 4, course_groups: [course_group_lab_sciences])
-course_eas1600 = safe_create Course.new(id: 11, name: 'EAS 1600', description: 'Earth and Atmospheric Sciences I', hours: 4, course_groups: [course_group_lab_sciences])
-course_eas1601 = safe_create Course.new(id: 12, name: 'EAS 1601', description: 'Earth and Atmospheric Sciences II', hours: 4, course_groups: [course_group_lab_sciences])
-course_cs1301 = safe_create Course.new(id: 13, name: 'CS 1301', description: 'Introduction to Computing and Programming', hours: 3)
-course_cs1331 = safe_create Course.new(id: 14, name: 'CS 1331', description: 'Introduction to Object-Oriented Programming', hours: 3)
-course_cs1332 = safe_create Course.new(id: 15, name: 'CS 1332', description: 'Data Structures and Algorithms', hours: 3)
-course_cs2340 = safe_create Course.new(id: 16, name: 'CS 2340', description: 'Objects and Design', hours: 3)
-course_cs2110 = safe_create Course.new(id: 17, name: 'CS 2110', description: 'Computing Organization and Programming', hours: 4)
-course_cs2200 = safe_create Course.new(id: 18, name: 'CS 2200', description: 'Computer Systems and Networks', hours: 4)
+course_math1551 = safe_create Course.new(id: 6, name: 'MATH 1551', description: 'Differential Calculus', hours: 3)
+course_math1552 = safe_create Course.new(id: 7, name: 'MATH 1552', description: 'Integral Calculus', hours: 3)
+course_math1554 = safe_create Course.new(id: 8, name: 'MATH 1554', description: 'Linear Algebra', hours: 3)
+course_math2550 = safe_create Course.new(id: 9, name: 'MATH 2550', description: 'Intro to Multivariable Calculus', hours: 3)
+course_math3012 = safe_create Course.new(id: 10, name: 'MATH 3012', description: 'Applied Combinatorics', hours: 3)
+course_math3215 = safe_create Course.new(id: 11, name: 'MATH 3215', description: 'Intro to Probability and Statistics', hours: 3)
+course_math3670 = safe_create Course.new(id: 12, name: 'MATH 3670', description: 'Probability and Statistics With Applications', hours: 3)
+course_isye3770 = safe_create Course.new(id: 13, name: 'ISYE 3770', description: 'Statistics and Applications', hours: 3)
+course_isye2027 = safe_create Course.new(id: 14, name: 'ISYE 2027', description: 'Probability With Applications', hours: 3)
+course_isye2028 = safe_create Course.new(id: 15, name: 'ISYE 2028', description: 'Basic Statistical Methods', hours: 3)
+course_phys2211 = safe_create Course.new(id: 16, name: 'PHYS 2211', description: 'Physics I', hours: 4, course_groups: [course_group_lab_sciences])
+course_phys2212 = safe_create Course.new(id: 17, name: 'PHYS 2212', description: 'Physics II', hours: 4, course_groups: [course_group_lab_sciences])
+course_chem1310 = safe_create Course.new(id: 18, name: 'CHEM 1310', description: 'General Chemistry', hours: 4, course_groups: [course_group_lab_sciences])
+course_chem1211 = safe_create Course.new(id: 19, name: 'CHEM 1211', description: 'Chemistry I', hours: 4, course_groups: [course_group_lab_sciences])
+course_chem1212 = safe_create Course.new(id: 20, name: 'CHEM 1212', description: 'Chemistry II', hours: 4, course_groups: [course_group_lab_sciences])
+course_eas1600 = safe_create Course.new(id: 21, name: 'EAS 1600', description: 'Earth and Atmospheric Sciences I', hours: 4, course_groups: [course_group_lab_sciences])
+course_eas1601 = safe_create Course.new(id: 22, name: 'EAS 1601', description: 'Earth and Atmospheric Sciences II', hours: 4, course_groups: [course_group_lab_sciences])
+course_cs1301 = safe_create Course.new(id: 23, name: 'CS 1301', description: 'Introduction to Computing and Programming', hours: 3)
+course_cs1331 = safe_create Course.new(id: 24, name: 'CS 1331', description: 'Introduction to Object-Oriented Programming', hours: 3)
+course_cs1332 = safe_create Course.new(id: 25, name: 'CS 1332', description: 'Data Structures and Algorithms', hours: 3)
+course_cs2340 = safe_create Course.new(id: 26, name: 'CS 2340', description: 'Objects and Design', hours: 3)
+course_cs2110 = safe_create Course.new(id: 27, name: 'CS 2110', description: 'Computing Organization and Programming', hours: 4)
+course_cs2200 = safe_create Course.new(id: 28, name: 'CS 2200', description: 'Computer Systems and Networks', hours: 4)
 
 # Prereqs
 prereq_eng1102 = safe_create Prereq.new(id: 0, op: 'and', courses: [course_eng1101])
@@ -58,11 +68,13 @@ prereq_eas1601 = safe_create Prereq.new(id: 5, op: 'and', courses: [course_eas16
 course_eas1601.update_attribute :prereq_id, prereq_eas1601.id
 
 # Requirements and rules
+# Humanities
 req_humanities = safe_create Requirement.new(id: 0, majors: [major_cs], priority: 0, op: 'and')
 rule_eng1101 = safe_create RequirementRule.new(id: 0, requirement: req_humanities, priority: 0, rule_type: 'course', course: course_eng1101) 
 rule_eng1102 = safe_create RequirementRule.new(id: 1, requirement: req_humanities, priority: 1, rule_type: 'course', course: course_eng1102) 
 rule_humanities_6hr = safe_create RequirementRule.new(id: 2, requirement: req_humanities, priority: 2, rule_type: 'hours', quantity: 6, course_group: course_group_humanities)
 
+# Lab sciences
 req_lab_sciences = safe_create Requirement.new(id: 2, majors: [major_cs], priority: 2, op: 'and')
 rule_phys2211 = safe_create RequirementRule.new(id: 3, requirement: req_lab_sciences, priority: 0, rule_type: 'course', course: course_phys2211)
 req_additional_lab_sciences = safe_create Requirement.new(id: 3, parent_id: req_lab_sciences.id, priority: 1, op: 'or')
@@ -75,3 +87,39 @@ rule_chem1212 = safe_create RequirementRule.new(id: 7, requirement: req_chem, pr
 req_eas = safe_create Requirement.new(id: 6, parent_id: req_additional_lab_sciences.id, priority: 2, op: 'and')
 rule_eas1600 = safe_create RequirementRule.new(id: 8, requirement: req_eas, priority: 0, rule_type: 'course', course: course_eas1600)
 rule_eas1601 = safe_create RequirementRule.new(id: 9, requirement: req_eas, priority: 1, rule_type: 'course', course: course_eas1601)
+
+# Mathematics
+req_math1551 = safe_create Requirement.new(id: 7, majors: [major_cs], priority: 3, op: 'and')
+rule_math1551 = safe_create RequirementRule.new(id: 10, requirement: req_math1551, priority: 0, rule_type: 'course', course: course_math1551)
+req_math1552 = safe_create Requirement.new(id: 8, majors: [major_cs], priority: 4, op: 'and')
+rule_math1552 = safe_create RequirementRule.new(id: 11, requirement: req_math1552, priority: 0, rule_type: 'course', course: course_math1552)
+req_math1554 = safe_create Requirement.new(id: 9, majors: [major_cs], priority: 5, op: 'and')
+rule_math1554 = safe_create RequirementRule.new(id: 12, requirement: req_math1554, priority: 0, rule_type: 'course', course: course_math1554)
+req_math2550 = safe_create Requirement.new(id: 10, majors: [major_cs], priority: 6, op: 'and')
+rule_math2550 = safe_create RequirementRule.new(id: 13, requirement: req_math2550, priority: 0, rule_type: 'course', course: course_math2550)
+req_math3012 = safe_create Requirement.new(id: 11, majors: [major_cs], priority: 7, op: 'and')
+rule_math3012 = safe_create RequirementRule.new(id: 14, requirement: req_math3012, priority: 0, rule_type: 'course', course: course_math3012)
+req_stat = safe_create Requirement.new(id: 12, majors: [major_cs], priority: 8, op: 'or')
+rule_math3670 = safe_create RequirementRule.new(id: 15, requirement: req_stat, priority: 0, rule_type: 'course', course: course_math3670)
+rule_isye3770 = safe_create RequirementRule.new(id: 16, requirement: req_stat, priority: 1, rule_type: 'course', course: course_isye3770)
+req_isye_stat = safe_create Requirement.new(id: 13, parent_id: req_stat.id, priority: 2, op: 'and')
+rule_isye2027 = safe_create RequirementRule.new(id: 17, requirement: req_isye_stat, priority: 0, rule_type: 'course', course: course_isye2027)
+rule_isye2028 = safe_create RequirementRule.new(id: 18, requirement: req_isye_stat, priority: 1, rule_type: 'course', course: course_isye2028)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
