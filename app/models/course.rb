38b2f079a6e0a50c3a408d66880a25620baebcef
Craig McCown <craigrmccown@gmail.com>
2016-04-25 00:53:09 -0400

    embedding sections

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,12 @@
 class Course < ActiveRecord::Base
   include JsonSupport
 
+  json_embed :sections
+
   validates :name, presence: true
   validates :description, presence: true
 
+  has_many :sections
   has_and_belongs_to_many :course_groups
   has_and_belongs_to_many :prereqs
   has_and_belongs_to_many :majors
